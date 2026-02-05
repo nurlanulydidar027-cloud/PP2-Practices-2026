db0a95dc14d8da2a728519e5c8ccb55364fb0749
didar nurlanuly <nurlanulydidar027@gmail.com>
2026-02-05 10:44:57 +0500

    added compare

---
+++ PP2_Practices-2026/Practice_2/boolean.py
@@ -0,0 +1,22 @@
+# boolean basic
+
+a = True
+b = False
+
+print("a =", a)
+print("b =", b)
+
+# data type
+print(type(a))
+
+
+
+# boolean compare
+
+x = 10
+y = 5
+
+print(x > y)   # True
+print(x < y)   # False
+print(x == y)  # False
+print(x != y)  # True
