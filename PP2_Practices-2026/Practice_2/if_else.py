4eca563a4d120b647d0e2b5ee2a8df31c8edeebb
didar nurlanuly <nurlanulydidar027@gmail.com>
2026-02-05 11:11:09 +0500

    add if/else/elif examples

---
+++ PP2_Practices-2026/Practice_2/if_else.py
@@ -0,0 +1,62 @@
+# is the number even or odd?
+
+num = 8
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+
+# voters over 18
+
+age = 17
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You are too young to vote")
+
+
+
+# check if item is in the list
+
+fruits = ["apple", "banana", "orange"]
+fruit = "banana"
+
+if fruit in fruits:
+    print(fruit, "is in the list")
+else:
+    print(fruit, "is not in the list")
+
+
+
+# what is my grade?
+
+score = 85
+
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("F")
+
+
+
+# temperature check
+
+temp = 25
+
+if temp > 30:
+    print("It's hot")
+elif temp > 20:
+    print("It's warm")
+elif temp > 10:
+    print("It's cool")
+else:
+    print("It's cold")
+
