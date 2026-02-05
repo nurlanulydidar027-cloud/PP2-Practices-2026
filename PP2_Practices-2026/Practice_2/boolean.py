6967ae37794d8f22fcdf5b9992560fd35f50e176
didar nurlanuly <nurlanulydidar027@gmail.com>
2026-02-05 10:52:34 +0500

    5 examples

--- PP2_Practices-2026/Practice_2/boolean.py
+++ PP2_Practices-2026/Practice_2/boolean.py
@@ -20,3 +20,36 @@ print(x > y)   # True
 print(x < y)   # False
 print(x == y)  # False
 print(x != y)  # True
+
+
+
+# boolean if
+
+is_raining = True
+
+if is_raining:
+    print("Take umbrella ☔")
+else:
+    print("Go outside and enjoy the sun ☀️")
+
+
+
+# boolean input
+
+num = int(input("Enter number: "))
+
+is_even = (num % 2 == 0)
+
+print("Is even:", is_even) # True if even, False if odd
+
+
+
+# bool() function 
+
+x = "Hello"
+y = 15
+z = 0
+
+print(bool(x))
+print(bool(y))
+print(bool(z))
