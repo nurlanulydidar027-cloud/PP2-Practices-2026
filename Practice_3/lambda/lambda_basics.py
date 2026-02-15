020856e664b9a25e459bcdf494fedc49f3dadc18
didar nurlanuly <nurlanulydidar027@gmail.com>
2026-02-15 21:50:47 +0500

    update fuckin

---
+++ Practice_3/lambda/lambda_basics.py
@@ -0,0 +1,13 @@
+double = lambda x: x * 2
+
+
+add_three = lambda a, b, c: a + b + c
+
+
+power = lambda base, exp: base ** exp
+
+
+is_positive = lambda x: x > 0
+
+
+full_name = lambda first, last: f"{first} {last}"
