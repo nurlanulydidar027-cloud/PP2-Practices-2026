c275d8ee995a0b60ea00cd7ac907ad9445624f9e
didar nurlanuly <nurlanulydidar027@gmail.com>
2026-03-04 17:49:10 +0500

    fix peoblem with txt file

--- Practice_6/file_handing/read_files.py
+++ Practice_6/file_handing/read_files.py
@@ -1,27 +1,28 @@
 # Reading files in Python
 
 # 1. Read entire file
-with open("data.txt", "r") as f:
+with open("C:\\Users\\User\\kalmik\\PP2_Practices-2026\\Practice_6\\data.txt", "r") as f:
     content = f.read()
-    print(content)
+    print(content + '\n')  
 
 # 2. Read line by line
-with open("data.txt", "r") as f:
+with open("C:\\Users\\User\\kalmik\\PP2_Practices-2026\\Practice_6\\data.txt", "r") as f:
     for line in f:
-        print(line.strip())
+        print(line.strip() + '\n')  
 
 # 3. Read all lines into a list
-with open("data.txt", "r") as f:
+with open("C:\\Users\\User\\kalmik\\PP2_Practices-2026\\Practice_6\\data.txt", "r") as f:
     lines = f.readlines()
     print(lines)  # ['line1\n', 'line2\n', ...]
+    print()
 
 # 4. Read first N lines
-with open("data.txt", "r") as f:
+with open("C:\\Users\\User\\kalmik\\PP2_Practices-2026\\Practice_6\\data.txt", "r") as f:
     for i, line in enumerate(f):
-        if i == 3: break
-        print(line.strip())
+        if i == 2: break
+        print(line.strip() + '\n')
 
 # 5. Read with encoding
-with open("data.txt", "r", encoding="utf-8") as f:
+with open("C:\\Users\\User\\kalmik\\PP2_Practices-2026\\Practice_6\\data.txt", "r", encoding="utf-8") as f:
     content = f.read()
-    print(content)
+    print(content + '\n')
